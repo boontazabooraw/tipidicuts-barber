eded003478e0796bd769b358677563520738c1dd
Leon Bryan <leonard.bryan.email@gmail.com>
2025-09-24 01:07:06 +0800

    Fixed responsiveness on second section

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -4,52 +4,59 @@ import Image from "next/image";
 
 const AboutSection = () => {
   return (
-    <div className="section-wrapper px-5 md:px-60">
-      <div className="flex flex-col justify-center items-center h-full gap-7">
-        <div className="flex items-center">
-          <span className="font-bold text-strong md:text-center text-justify md:text-3xl text-xl font-culot leading-normal">
-            Tipidicuts is your neighborhood barbershop in Orion, Bataan — right
-            across Orion Plaza. We specialize in classic and modern cuts,
-            keeping it budget-friendly while making sure you walk out looking
-            sharp.
+    <div className="section-wrapper px-5 lg:px-60 md:px-20">
+      <div className="flex lg:flex-row flex-col justify-center items-center h-full gap-7 lg:gap-0">
+        <div className="flex items-center lg:w-150">
+          <span className="font-bold text-strong lg:text-end text-justify lg:text-3xl text-xl font-culot leading-relaxed tracking-wider">
+            <span className="text-blu opacity-90">Tipid</span>
+            <span className="text-rid opacity-90">icuts </span>
+            is your neighborhood barbershop in Orion, Bataan — right across
+            Orion Plaza. We specialize in classic and modern cuts, keeping it
+            budget-friendly while making sure you walk out looking sharp.
           </span>
         </div>
-        <div className="flex flex-wrap justify-around gap-7">
-          <div className="flex flex-col items-center justify-between">
+        <div className="flex flex-wrap lg:justify-center justify-around gap-7 lg:gap-0">
+          <div className="flex lg:flex-row flex-col items-center lg:justify-center justify-between">
             <Image
               src={"/images/logos/titotatay-trim.png"}
-              alt="Stellar logo"
-              width={130}
-              height={130}
+              alt="Tito / Tatay Trim"
+              width={200}
+              height={200}
               quality={100}
+              className="brightness-90 lg:w-[60%] w-33"
             />
-            <h1 className="text-3xl opacity-70 font-culot tracking-tighter uppercase">
-              Tito Trim
+            <h1 className="lg:text-4xl text-3xl opacity-90 font-culot tracking-tighter uppercase lg:[writing-mode:vertical-lr]">
+              <span className="text-blu">Tito</span>
+              <span className="text-rid">Trim</span>
             </h1>
           </div>
-          <div className="flex flex-col items-center justify-between">
+          <div className="flex lg:flex-row flex-col items-center lg:justify-center justify-between">
             <Image
               src={"/images/logos/bagets-style.png"}
-              alt="Stellar logo"
-              width={150}
-              height={150}
+              alt="Bagets style"
+              width={200}
+              height={200}
               quality={100}
+              className="brightness-90 lg:w-[66%] w-40"
             />
-            <h1 className="text-3xl opacity-70 font-culot tracking-tighter uppercase">
-              Bagets Style
+            <h1 className="lg:text-4xl text-3xl opacity-90 font-culot tracking-tighter uppercase lg:[writing-mode:vertical-lr]">
+              <span className="text-blu">Bagets</span>
+              <span className="text-rid">Style</span>
             </h1>
           </div>
-          <div className="flex flex-col items-center justify-between">
+          <div className="flex lg:flex-row flex-col items-center justify-between lg:justify-center">
             <Image
               src={"/images/logos/astig-fade.png"}
-              alt="Stellar logo"
-              width={150}
-              height={150}
+              alt="Astig Fade"
+              width={200}
+              height={200}
               quality={100}
-              // className="md:w-[30%] w-[40%] h-auto"
+              className="brightness-90 lg:w-[66%] w-40"
+              // className="lg:w-[30%] w-[40%] h-auto"
             />
-            <h1 className="text-3xl opacity-70 font-culot tracking-tighter uppercase">
-              Astig Fade
+            <h1 className="lg:text-4xl text-3xl opacity-90 font-culot tracking-tighter uppercase lg:[writing-mode:vertical-lr]">
+              <span className="text-blu">Astig</span>
+              <span className="text-rid">Fade</span>
             </h1>
           </div>
         </div>
